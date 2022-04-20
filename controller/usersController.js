84bd363bda62e6be379f66fe4439713b675fe940
Muzahidul <bekon28@outlook.com>
2022-04-20 12:34:22 +0600

    user validation, user avatar upload,  user data model, and split input from user file

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,7 +1,45 @@
+//external import
+const bcrypt = require("bcrypt");
+
+// internal import
+const User = require("../models/People");
+
 function getUsers(req, res) {
   res.render("users");
+}
+async function addUser(req, res, next) {
+  let newUser;
+  const hashPassword = await bcrypt.hash(req.body.password, 10);
+  if (req.files && req.files.length > 0) {
+    newUser = new User({
+      ...req.body,
+      avatar: req.files[0].filename,
+      password: hashPassword,
+    });
+  } else {
+    newUser = new User({
+      ...req.body,
+      password: hashPassword,
+    });
+  }
+
+  try {
+    const result = await newUser.save();
+    res.status(200).json({
+      message: "User was added successfully!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: {
+        common: {
+          msg: "Unknown error occurred!",
+        },
+      },
+    });
+  }
 }
 
 module.exports = {
   getUsers,
+  addUser,
 };
--- router/userRouter.js
+++ router/userRouter.js
@@ -5,8 +5,25 @@ const router = express.Router();
 // internal export
 const { getUsers } = require("../controller/usersController");
 const setLocalVar = require("../middlewares/common/localVar");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const { addUser } = require("../controller/usersController");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidator");
+
 //Users page
 
 router.get("/", setLocalVar("User"), getUsers);
+
+// add user
+
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 module.exports = router;
